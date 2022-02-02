f48985d72fb1c13c9c2dcb0e8a63058488668722
rdwheeler <rdwheeler@wpi.edu>
2022-02-01 21:57:34 -0500

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         MapInitializer mapInitializer = new MapInitializer(args[0]);
         int h = Integer.parseInt(args[1]);
         Search search = new Search(h);
-        final long startTime = System.nanoTime();
+        //final long startTime = System.nanoTime();
 
         search.AStar(mapInitializer.getMap());
 
-        final long duration = System.nanoTime() - startTime;
-        System.out.println(duration);
-        System.out.println((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+        //final long duration = System.nanoTime() - startTime;
+        //System.out.println(duration);
+        //System.out.println((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
     }
 
 }
